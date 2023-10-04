b7d34d45b3250cea9c5bed9552003b129b44a5af
Just-Ipavon~ <pachiruo@gmail.com>
2023-10-04 12:56:03 +0200

    es

--- ALGORITMI E STRUTTURE DATI/src/Esercizio File/file.cpp
+++ ALGORITMI E STRUTTURE DATI/src/Esercizio File/file.cpp
@@ -57,3 +57,25 @@ int main(){
     rispetta_i_versi.verifica_soluzioni();
     return 0;
 }
+
+/*
+Il codice sopra è un'implementazione in C++ di una classe chiamata RispettaIVersi che legge due file di input, 
+problema.txt e soluzione.txt, e scrive i risultati della verifica delle soluzioni su un file di output, esito.txt. 
+Il problema che viene risolto consiste nel verificare se ogni soluzione in soluzione.txt è corretta rispetto al problema corrispondente in problema.txt. 
+Il problema consiste in una sequenza di segni < e >, e la soluzione corretta è una sequenza di interi che soddisfa i segni.
+
+La classe RispettaIVersi ha tre variabili membro private: problema_file_, soluzione_file_ e esito_file_, 
+che sono i nomi dei file di input e output. Il costruttore prende questi tre nomi di file come argomenti e inizializza le variabili membro. 
+La classe ha anche due funzioni membro private: verifica_soluzione e verifica_soluzioni. La prima prende un problema e 
+una soluzione come stringhe e restituisce un booleano che indica se la soluzione è corretta rispetto al problema. La seconda legge i problemi e le soluzioni dai file di input, 
+verifica ogni soluzione e scrive i risultati sul file di output.
+
+La funzione verifica_soluzione crea prima un vettore di interi da 1 alla lunghezza della stringa soluzione. Poi itera sulla stringa problema e 
+controlla ogni segno rispetto alla coppia corrispondente di interi nella stringa soluzione. Se il segno è < e l'intero a sinistra è maggiore o uguale all'intero a destra,
+ o se il segno è > e l'intero a sinistra è minore o uguale all'intero a destra, la funzione restituisce false. Altrimenti, restituisce true.
+
+La funzione verifica_soluzioni apre prima i file di input e output utilizzando ifstream e ofstream, rispettivamente. Poi legge ogni riga dai file di input utilizzando getline, 
+verifica la soluzione corrispondente utilizzando verifica_soluzione e scrive il risultato sul file di output utilizzando <<. Infine, chiude tutti i file utilizzando close.
+
+La funzione main crea un'istanza della classe RispettaIVersi con i nomi dei file di input e output, chiama il metodo verifica_soluzioni e restituisce 0 per indicare l'esecuzione corretta.
+*/
